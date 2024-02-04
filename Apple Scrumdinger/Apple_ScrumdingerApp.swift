498a0cd15d667a0e560e8303e8afb56f7cc8918d
sinceredevotion <yuriibibik@gmail.com>
2024-02-04 12:26:31 -0800

    Initial Commit

---
+++ Apple Scrumdinger/Apple_ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_ScrumdingerApp.swift
+//  Apple Scrumdinger
+//
+//  Created by Yurii Bibik on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple Scrumdinger/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Apple Scrumdinger
+//
+//  Created by Yurii Bibik on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
